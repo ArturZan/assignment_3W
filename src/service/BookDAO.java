59a2fe8ae0d40c2c82b893717db5275a24f924a2
ArturZan <165910566+ArturZan@users.noreply.github.com>
2026-01-29 10:11:01 +0500

    Update BookDAO.java

--- src/service/BookDAO.java
+++ src/service/BookDAO.java
@@ -11,203 +11,203 @@ import java.util.List;
 
 
 public class BookDAO {
-    public void addEBook(EBook book) {
-        String sql = "insert into books(title, isbn,author_id, publish_year, book_type, download_url, file_size) values(?,?,?,?,'EBOOK',?,?)";
+    public void addEBook(EBook bookToAdd) {
+        String insertQuery = "insert into books(title, isbn,author_id, publish_year, book_type, download_url, file_size) values(?,?,?,?,'EBOOK',?,?)";
 
-        try (Connection connection = DatabaseConnection.getConnection();
-        PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setString(1, book.getTitle());
-            ps.setString(2, book.getIsbn());
-            ps.setInt(3,book.getAuthor().getId());
-            ps.setInt(4, book.getYear());
-            ps.setString(5, book.getDownloadURL());
-            ps.setDouble(6, book.getFileSize());
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
+        try (Connection dbConnection = DatabaseConnection.getConnection();
+        PreparedStatement statement = dbConnection.prepareStatement(insertQuery)) {
+            statement.setString(1, bookToAdd.getTitle());
+            statement.setString(2, bookToAdd.getIsbn());
+            statement.setInt(3, bookToAdd.getAuthor().getId());
+            statement.setInt(4, bookToAdd.getYear());
+            statement.setString(5, bookToAdd.getDownloadURL());
+            statement.setDouble(6, bookToAdd.getFileSize());
+            statement.executeUpdate();
+        } catch (SQLException sqlException) {
+            System.err.println(sqlException.getMessage());
         }
     }
 
-   public void addPrintedBook(printedBook book) {
-        String sql = "insert into books(title, isbn, author_id, publish_year, book_type, shelf_location, weight, available) " +
+   public void addPrintedBook(printedBook bookToAdd) {
+        String insertQuery = "insert into books(title, isbn, author_id, publish_year, book_type, shelf_location, weight, available) " +
                 "values(?, ?, ?, ?, 'PRINTED', ?, ?, ?)";
 
-        try (Connection connection = DatabaseConnection.getConnection();
-        PreparedStatement ps = connection.prepareStatement(sql)){
-            ps.setString(1, book.getTitle());
-            ps.setString(2, book.getIsbn());
-            ps.setInt(3, book.getAuthor().getId());
-            ps.setInt(4,book.getYear());
-            ps.setString(5, book.getShelfLocation());
-            ps.setDouble(6, book.getWeight());
-            ps.setBoolean(7, book.isAvailable());
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
+        try (Connection dbConnection = DatabaseConnection.getConnection();
+        PreparedStatement statement = dbConnection.prepareStatement(insertQuery)){
+            statement.setString(1, bookToAdd.getTitle());
+            statement.setString(2, bookToAdd.getIsbn());
+            statement.setInt(3, bookToAdd.getAuthor().getId());
+            statement.setInt(4, bookToAdd.getYear());
+            statement.setString(5, bookToAdd.getShelfLocation());
+            statement.setDouble(6, bookToAdd.getWeight());
+            statement.setBoolean(7, bookToAdd.isAvailable());
+            statement.executeUpdate();
+        } catch (SQLException sqlException) {
+            System.err.println(sqlException.getMessage());
         }
    }
 
-    public Book getBookByID(int id) {
-        String sql = "select * from books where id = ?";
-        try (Connection connection = DatabaseConnection.getConnection();
-            PreparedStatement ps = connection.prepareStatement(sql))  {
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
+    public Book getBookByID(int bookId) {
+        String selectQuery = "select * from books where id = ?";
+        try (Connection dbConnection = DatabaseConnection.getConnection();
+            PreparedStatement statement = dbConnection.prepareStatement(selectQuery))  {
+            statement.setInt(1, bookId);
+            ResultSet queryResult = statement.executeQuery();
 
 
-            if (rs.next()) {
-                String bookType = rs.getString("book_type");
+            if (queryResult.next()) {
+                String typeOfBook = queryResult.getString("book_type");
 
-                if ("EBOOK".equals(bookType)) {
-                    return createEBookFromResultSet(rs);
-                } else if("PRINTED".equals((bookType))) {
-                    return createPrintedBookFromResultSet(rs);
+                if ("EBOOK".equals(typeOfBook)) {
+                    return createEBookFromResultSet(queryResult);
+                } else if("PRINTED".equals((typeOfBook))) {
+                    return createPrintedBookFromResultSet(queryResult);
                 }
             } else {
                 return null;
             }
-        } catch (SQLException e) {
-            System.err.println("Error" + e.getMessage());
+        } catch (SQLException sqlException) {
+            System.err.println("Error" + sqlException.getMessage());
             return null;
         }
         return null;
     }
 
-    public EBook createEBookFromResultSet(ResultSet rs) throws SQLException {
-        int id = rs.getInt("id");
-        String title = rs.getString("title");
-        String isbn = rs.getString("isbn");
-        int authorId = rs.getInt("author_id");
-        int year = rs.getInt("publish_year");
-        String downloadUrl = rs.getString("download_url");
-        double fileSize = rs.getDouble("file_size");
+    public EBook createEBookFromResultSet(ResultSet resultData) throws SQLException {
+        int bookId = resultData.getInt("id");
+        String bookTitle = resultData.getString("title");
+        String bookIsbn = resultData.getString("isbn");
+        int authorIdentifier = resultData.getInt("author_id");
+        int publicationYear = resultData.getInt("publish_year");
+        String urlForDownload = resultData.getString("download_url");
+        double sizeOfFile = resultData.getDouble("file_size");
 
-        AuthorDAO authorDAO = new AuthorDAO();
-        Author author = authorDAO.getAuthorByID(authorId);
+        AuthorDAO authorDataAccess = new AuthorDAO();
+        Author bookAuthor = authorDataAccess.getAuthorByID(authorIdentifier);
 
-        EBook ebook = new EBook(id, title, author, year, isbn, fileSize, downloadUrl);
-        return ebook;
+        EBook electronicBook = new EBook(bookId, bookTitle, bookAuthor, publicationYear, bookIsbn, sizeOfFile, urlForDownload);
+        return electronicBook;
     }
 
-    public printedBook createPrintedBookFromResultSet(ResultSet rs) throws SQLException {
-        int id = rs.getInt("id");
-        String title = rs.getString("title");
-        String isbn = rs.getString("isbn");
-        int authorId = rs.getInt("author_id");
-        int year = rs.getInt("publish_year");
-        String shelflocation = rs.getString("shelf_location");
-        double weight = rs.getDouble("weight");
+    public printedBook createPrintedBookFromResultSet(ResultSet resultData) throws SQLException {
+        int bookId = resultData.getInt("id");
+        String bookTitle = resultData.getString("title");
+        String bookIsbn = resultData.getString("isbn");
+        int authorIdentifier = resultData.getInt("author_id");
+        int publicationYear = resultData.getInt("publish_year");
+        String locationOnShelf = resultData.getString("shelf_location");
+        double bookWeight = resultData.getDouble("weight");
 
-        AuthorDAO authorDAO = new AuthorDAO();
-        Author author = authorDAO.getAuthorByID(authorId);
-        printedBook pbook = new printedBook(id, title, author, year, isbn, shelflocation, weight);
-        return pbook;
+        AuthorDAO authorDataAccess = new AuthorDAO();
+        Author bookAuthor = authorDataAccess.getAuthorByID(authorIdentifier);
+        printedBook physicalBook = new printedBook(bookId, bookTitle, bookAuthor, publicationYear, bookIsbn, locationOnShelf, bookWeight);
+        return physicalBook;
     }
 
     public List<Book> getAllBooks() {
-        String sql = "select * from books";
-        List<Book> books = new ArrayList<>();
+        String selectAllQuery = "select * from books";
+        List<Book> bookCollection = new ArrayList<>();
 
-        try (Connection connection = DatabaseConnection.getConnection();
-        PreparedStatement ps = connection.prepareStatement(sql)) {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                String bookType = rs.getString("book_type");
+        try (Connection dbConnection = DatabaseConnection.getConnection();
+        PreparedStatement statement = dbConnection.prepareStatement(selectAllQuery)) {
+            ResultSet queryResult = statement.executeQuery();
+            while (queryResult.next()) {
+                String typeOfBook = queryResult.getString("book_type");
 
-                if ("EBOOK".equals(bookType)) {
-                    books.add(createEBookFromResultSet(rs));
-                } else if ("PRINTED".equals(bookType)) {
-                    books.add(createPrintedBookFromResultSet(rs));
+                if ("EBOOK".equals(typeOfBook)) {
+                    bookCollection.add(createEBookFromResultSet(queryResult));
+                } else if ("PRINTED".equals(typeOfBook)) {
+                    bookCollection.add(createPrintedBookFromResultSet(queryResult));
                 }
             }
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
+        } catch (SQLException sqlException) {
+            System.err.println(sqlException.getMessage());
         }
-        return books;
+        return bookCollection;
 
     }
 
-    public Book getBookByISBN(String isbn) {
-        String sql = "select * from books where isbn = ?";
-        try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, isbn);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                String bookType = rs.getString("book_type");
-                if ("EBOOK".equals(bookType)) {
-                    return createEBookFromResultSet(rs);
-                } else if ("PRINTED".equals(bookType)) {
-                    return createPrintedBookFromResultSet(rs);
+    public Book getBookByISBN(String isbnCode) {
+        String selectQuery = "select * from books where isbn = ?";
+        try (Connection dbConnection = DatabaseConnection.getConnection();
+             PreparedStatement statement = dbConnection.prepareStatement(selectQuery)) {
+            statement.setString(1, isbnCode);
+            ResultSet queryResult = statement.executeQuery();
+            if (queryResult.next()) {
+                String typeOfBook = queryResult.getString("book_type");
+                if ("EBOOK".equals(typeOfBook)) {
+                    return createEBookFromResultSet(queryResult);
+                } else if ("PRINTED".equals(typeOfBook)) {
+                    return createPrintedBookFromResultSet(queryResult);
                 }
             }
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
+        } catch (SQLException sqlException) {
+            System.err.println(sqlException.getMessage());
         }
         return null;
     }
 
-    public void updateBookByID(int id, String newTitle, String newIsbn, int newAuthorId, int newPublishYear, double newFileSize,String newDownloadUrl ,String newShelfLocation, double newWeight) {
-        String sql1 = "update table books set title = ?,isbn =?, author_id = ?, publish_year = ?, file_size=?,download_url = ?, shelf_locaion=?, weight = ?  where id = ?";
-        String sql2 = "select book_type from books where id = ?";
-        try (Connection connection = DatabaseConnection.getConnection();
-        PreparedStatement psCheckId = connection.prepareStatement(sql2);
-        PreparedStatement psUpdate = connection.prepareStatement(sql1);) {
+    public void updateBookByID(int bookId, String updatedTitle, String updatedIsbn, int updatedAuthorId, int updatedPublishYear, double updatedFileSize, String updatedDownloadUrl, String updatedShelfLocation, double updatedWeight) {
+        String updateQuery = "update table books set title = ?,isbn =?, author_id = ?, publish_year = ?, file_size=?,download_url = ?, shelf_locaion=?, weight = ?  where id = ?";
+        String selectTypeQuery = "select book_type from books where id = ?";
+        try (Connection dbConnection = DatabaseConnection.getConnection();
+        PreparedStatement statementCheckId = dbConnection.prepareStatement(selectTypeQuery);
+        PreparedStatement statementUpdate = dbConnection.prepareStatement(updateQuery);) {
 
-            String book_type;
-            psCheckId.setInt(1, id);
-            ResultSet rs = psCheckId.executeQuery();
-            if (rs.next()) {
-                book_type = rs.getString("book_type");
-                if ("EBOOK".equals(book_type)) {
-                    psUpdate.setString(1, newTitle);
-                    psUpdate.setString(2, newIsbn);
-                    psUpdate.setInt(3, newAuthorId);
-                    psUpdate.setInt(4, newPublishYear);
-                    psUpdate.setDouble(5,newFileSize);
-                    psUpdate.setString(6, newDownloadUrl);
-                    psUpdate.setNull(7, Types.DOUBLE);
-                    psUpdate.setNull(8, Types.VARCHAR); //null because ebook does not have shelflocation and weight
+            String categoryOfBook;
+            statementCheckId.setInt(1, bookId);
+            ResultSet queryResult = statementCheckId.executeQuery();
+            if (queryResult.next()) {
+                categoryOfBook = queryResult.getString("book_type");
+                if ("EBOOK".equals(categoryOfBook)) {
+                    statementUpdate.setString(1, updatedTitle);
+                    statementUpdate.setString(2, updatedIsbn);
+                    statementUpdate.setInt(3, updatedAuthorId);
+                    statementUpdate.setInt(4, updatedPublishYear);
+                    statementUpdate.setDouble(5, updatedFileSize);
+                    statementUpdate.setString(6, updatedDownloadUrl);
+                    statementUpdate.setNull(7, Types.DOUBLE);
+                    statementUpdate.setNull(8, Types.VARCHAR); //null because ebook does not have shelflocation and weight
 
-                    //id of a book
-                    psUpdate.setInt(9, id);
+            
+                    statementUpdate.setInt(9, bookId);
                 }
-                else if ("PRINTEDBOOK".equals(book_type)) {
-                    psUpdate.setString(1, newTitle);
-                    psUpdate.setString(2, newIsbn);
-                    psUpdate.setInt(3, newAuthorId);
-                    psUpdate.setInt(4, newPublishYear);
-                    psUpdate.setNull(5, Types.DOUBLE); //null because printed book does not have filesize and download url
-                    psUpdate.setNull(6, Types.VARCHAR);
-                    psUpdate.setString(7,newShelfLocation);
-                    psUpdate.setDouble(8, newWeight);
-                    //id of a book
-                    psUpdate.setInt(9, id);
+                else if ("PRINTEDBOOK".equals(categoryOfBook)) {
+                    statementUpdate.setString(1, updatedTitle);
+                    statementUpdate.setString(2, updatedIsbn);
+                    statementUpdate.setInt(3, updatedAuthorId);
+                    statementUpdate.setInt(4, updatedPublishYear);
+                    statementUpdate.setNull(5, Types.DOUBLE); //null because printed book does not have filesize and download url
+                    statementUpdate.setNull(6, Types.VARCHAR);
+                    statementUpdate.setString(7, updatedShelfLocation);
+                    statementUpdate.setDouble(8, updatedWeight);
+        
+                    statementUpdate.setInt(9, bookId);
                 }
                 else {
                     return;
                 }
             }
-            int rowsAffected = psUpdate.executeUpdate();
-            if(rowsAffected>0) {
+            int affectedRows = statementUpdate.executeUpdate();
+            if(affectedRows > 0) {
                 System.out.println("Book updated successfully");
             } else {
                 System.out.println("There is no book with this id");
             }
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
+        } catch (SQLException sqlException) {
+            System.err.println(sqlException.getMessage());
         }
     }
 
-    public void deleteBookById(int id) {
-        String sql = "delete from books where id = ?";
+    public void deleteBookById(int bookId) {
+        String deleteQuery = "delete from books where id = ?";
 
-        try (Connection connection = DatabaseConnection.getConnection();
-        PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setInt(1, id);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
+        try (Connection dbConnection = DatabaseConnection.getConnection();
+        PreparedStatement statement = dbConnection.prepareStatement(deleteQuery)) {
+            statement.setInt(1, bookId);
+            statement.executeUpdate();
+        } catch (SQLException sqlException) {
+            System.err.println(sqlException.getMessage());
         }
     }
 }
